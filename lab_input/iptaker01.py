eddb876fbaeb6e40e239a8f1fdb0a86b128b07a9
efloamazon <efloamazon@users.noreply.github.com>
2022-08-10 15:13:27 +0000

    learning about input function

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,12 @@
+#!/usr/bib/env python3
+
+"""Alta3 Research | RZFreezer
+    print() - display data to std out"""
+
+def main():
+
+    user_input = input("Please enter an IPv4 IP address")
+
+    print("You told me your IP adress is: " + user_input)
+
+main()
---
+++ lab_input/iptaker02.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Alta3 Reserach | RZFreeser
+    print() - concatenate vs print a series of items"""
+
+def main():
+
+    user_input = input("please enter an IP address:")
+
+    print("You gave me the an IP of:", user_input)
+
+main()
