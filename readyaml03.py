4f0fc1e42134db311fbe17f1aa2062ddb7b1c024
efloamazon <efloamazon@users.noreply.github.com>
2022-08-16 17:19:25 +0000

    learning about YAML

---
+++ readyaml03.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import yaml
+
+def main():
+    with open("myYAML.yml", "r") as yf:
+        pyyammy = yaml.load(yf, Loader=yaml.FullLoader)
+    print(pyyammy)
+
+if __name__ == "__main__":
+    main()
